c4291691f788f96e37db9f4ccfc515de3f64cf30
CCianfloneDev <colecianflone123@gmail.com>
2023-11-16 14:06:33 -0600

    Pulling only 3 columns for all rows

--- Weather Processing/scrape_weather.py
+++ Weather Processing/scrape_weather.py
@@ -19,10 +19,15 @@ class MyHTMLParser(HTMLParser):
 
         super().__init__()
         self.date_found = False
+        self.in_table_body = False
+        self.in_row_header = False
         self.in_row = False
-        self.in_col = False
-        self.in_rowheader = False
-        self.scraped = {}
+        self.in_row_data = False
+        self.row_column_index = 0
+        self.end_of_table = False
+        
+        #self.in_col = False
+        #self.scraped = {}
         self.start_date = "2013-5-"
         self.weather = []
         self.daily_temps = []
@@ -30,30 +35,57 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         """The tag to begin parsing at."""
         
+        if tag == "tbody":
+            self.in_table_body = True
+            
+        if tag == "tr" and self.in_table_body:
+            self.in_row = True
+        
+        if self.in_row and tag == "td":
+            self.in_row_data = True
+        
         # find the table row header
         if tag == "th":
             for attr, value in attrs:
                 # if its attribute is scope and the value is row
                 if attr == "scope" and 'row' in value:
                     # set the flag
-                    self.in_rowheader = True
+                    self.in_row_header = True
                 
         # if we're in the table row header
-        if self.in_rowheader == True:
+        if self.end_of_table == False and self.in_row_header == True:
             # and the element is abbr
             if tag == "abbr":
                 for attr, value in attrs:
                     # if the attribute is title
                     if attr == "title":
                         # get the date value and append it to our daily_temps list                                
+                        print(value)
                         converted_date = is_valid_date(value)
                         
-                        if converted_date is not None:                   
+                        if converted_date is not None:
                             self.weather.append(converted_date)
-
 
     def handle_data(self, data):
         """Look for ip in the data of the element."""
+        
+        if self.in_row_header == True:
+            if data == 'Sum':
+                self.end_of_table = True
+                self.reset_flags()
+        
+        # if we're in a datarow (<td>) and our counter is less than 3
+        if self.end_of_table == False and self.in_row_data == True and self.row_column_index <= 3:
+            if is_float(data) or data == 'M':
+                # we are inside of a <td> element datarow
+                self.row_column_index += 1
+                print(data)
+                                
+        # we are only looking for min, max, and mean, which are the first 3 columns of the table.
+        if self.in_row_data == True and self.row_column_index == 3:
+            # if we've hit 3 columns we need to reset our flags to move to the next row element.
+            self.reset_flags()
+            
         # self.daily_temps = {}
         
         # if self.in_row:
@@ -62,7 +94,14 @@ class MyHTMLParser(HTMLParser):
             
         #     for type_string in ip_addresses:
         #         self.daily_temps[type_string] = type_string
-        #     self.scraped[self.date_found] = self.daily_temps     
+        #     self.scraped[self.date_found] = self.daily_temps  
+        
+    def reset_flags(self):
+        """Resets the boolean flags indicating we are at a new row in the table."""    
+        self.in_row = False
+        self.in_row_data = False
+        self.in_row_header = False
+        self.row_column_index = 0
 
 
 class WeatherScraper:
@@ -81,7 +120,7 @@ class WeatherScraper:
             with urllib.request.urlopen(search_url) as response:
                 HTML_DATA = str(response.read())
                 self.parser.feed(HTML_DATA)
-                print(self.parser.weather)
+                #print(self.parser.weather)
         except HTTPError as e:
             print("HTTP Error:", e)
         except URLError as e:
@@ -99,6 +138,14 @@ def is_valid_date(value):
         # Return None if the value is not in the expected date format
         return None
 
+def is_float(string):
+    """Indicates if the passed string is a float."""
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+    
 if __name__ == "__main__":
     searchUrl = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5'
     weatherScraper = WeatherScraper()
