bf263acb6de634e7ba990a127970543a87a69e93
CCianfloneDev <colecianflone123@gmail.com>
2023-11-16 13:00:08 -0600

    Added checking and converting date

--- Weather Processing/scrape_weather.py
+++ Weather Processing/scrape_weather.py
@@ -4,11 +4,11 @@
 # Oct 12, 2023
 # ADEV-3005 241066
 
-
 import re
 from html.parser import HTMLParser
 import urllib.request
 from urllib.error import URLError, HTTPError
+from datetime import datetime
 
 
 class MyHTMLParser(HTMLParser):
@@ -24,6 +24,7 @@ class MyHTMLParser(HTMLParser):
         self.in_rowheader = False
         self.scraped = {}
         self.start_date = "2013-5-"
+        self.weather = []
         self.daily_temps = []
 
     def handle_starttag(self, tag, attrs):
@@ -44,17 +45,12 @@ class MyHTMLParser(HTMLParser):
                 for attr, value in attrs:
                     # if the attribute is title
                     if attr == "title":
-                        # get the date value and append it to our daily_temps list
+                        # get the date value and append it to our daily_temps list                                
+                        converted_date = is_valid_date(value)
                         
-                        # TODO: before adding it to list, check if its a date - if so, convert it to this format: YYYY-MM-DD instead of-
-                        #what is scraped: 'May 1, 2018'
-                        
-                        #print(value)
-                        self.daily_temps.append(value)
-                        
-                        
-        # elif tag == "a" and self.in_row:
-        #     self.in_a = True
+                        if converted_date is not None:                   
+                            self.weather.append(converted_date)
+
 
     def handle_data(self, data):
         """Look for ip in the data of the element."""
@@ -66,7 +62,7 @@ class MyHTMLParser(HTMLParser):
             
         #     for type_string in ip_addresses:
         #         self.daily_temps[type_string] = type_string
-        #     self.scraped[self.date_found] = self.daily_temps
+        #     self.scraped[self.date_found] = self.daily_temps     
 
 
 class WeatherScraper:
@@ -85,12 +81,23 @@ class WeatherScraper:
             with urllib.request.urlopen(search_url) as response:
                 HTML_DATA = str(response.read())
                 self.parser.feed(HTML_DATA)
-                print(self.parser.daily_temps)
+                print(self.parser.weather)
         except HTTPError as e:
             print("HTTP Error:", e)
         except URLError as e:
             print("URL Error:", e)
 
+def is_valid_date(value):
+    """Attempts to parse the value as a date in the 'YYYY-MM-DD' format."""
+    try:
+        # Parse the value as a date in the 'Month Day, Year' format
+        parsed_date = datetime.strptime(value, '%B %d, %Y')
+        # Convert the parsed date to 'YYYY-MM-DD' format
+        converted_date = parsed_date.strftime('%Y-%m-%d')
+        return converted_date
+    except ValueError:
+        # Return None if the value is not in the expected date format
+        return None
 
 if __name__ == "__main__":
     searchUrl = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5'
