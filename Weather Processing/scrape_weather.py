fcd7ff2a7500a2210f9d8313509d797497f21f64
CCianfloneDev <colecianflone123@gmail.com>
2023-11-16 16:00:09 -0600

    Working on incrementing page

--- Weather Processing/scrape_weather.py
+++ Weather Processing/scrape_weather.py
@@ -9,129 +9,161 @@ import urllib.request
 from urllib.error import URLError, HTTPError
 from datetime import datetime
 
-
-class MyHTMLParser(HTMLParser):
-    """The web scraper."""
-
-    def __init__(self):
-        """Instantiates the class and creates the ip found flag"""
-
-        super().__init__()
-        # flag properties
-        self.in_table_body = False
-        self.in_row_header = False
-        self.in_row = False
-        self.in_row_data = False
-        self.end_of_table = False
-        # row column counter
-        self.row_column_index = 0
-
-        # master dictionary
-        self.weather = {}
-        self.daily_temps = {} # dictionary stored inside of the master dictionary.
-
-        # dictionaries to be populated/accessed on each row iteration.
-        self.temporary_daily_dict = {}
-        self.column_temperature_legend = {0:"Max", 1:"Min", 2:"Mean"}
-        # property to store the row date for each row iteration
-        self.row_date = ""
-
-    def handle_starttag(self, tag, attrs):
-        """The tag to begin parsing at."""
-
-        if tag == "tbody":
-            self.in_table_body = True
-
-        if tag == "tr" and self.in_table_body:
-            self.in_row = True
-
-        if self.in_row and tag == "td":
-            self.in_row_data = True
-
-        # find the table row header
-        if tag == "th":
-            for attr, value in attrs:
-                # if its attribute is scope and the value is row
-                if attr == "scope" and 'row' in value:
-                    # set the flag
-                    self.in_row_header = True
-
-        # if we're in the table row header
-        if self.end_of_table is False and self.in_row_header is True:
-            # and the element is abbr
-            if tag == "abbr":
-                for attr, value in attrs:
-                    # if the attribute is title
-                    if attr == "title":
-                        # get the date value and append it to our daily_temps list
-                        converted_date = is_valid_date(value)
-
-                        if converted_date is not None:
-                            self.row_date = converted_date
-
-    def handle_data(self, data):
-        """Look for ip in the data of the element."""
-
-        if self.in_row_header is True:
-            if data == "Sum":
-                self.end_of_table = True
-
-                # TODO: make function that accept a dict and prints it out.
-                # uncomment to print the formatted dictionary
-                # print('{')
-                # for date, data in self.weather.items():
-                #     print(f"    '{date}': {data},")
-                # print('}')
-                # self.reset_flags()
-
-        # if we're in a data-row (<td>) and our counter is less than 3
-        if (self.end_of_table is False and self.in_row_data is True and self.row_column_index <= 3):
-            if is_float(data) or data == "M":
-                # we are inside of a <td> element data-row
-
-                # if self.row_column_index == 1:
-                self.temporary_daily_dict[
-                    self.column_temperature_legend.get(self.row_column_index)] = data
-                self.row_column_index += 1
-
-        # we are only looking for min, max, and mean, which are the first 3 columns of the table.
-        if self.in_row_data is True and self.row_column_index == 3:
-            # if we've hit 3 columns we need to reset our flags to move to the next row element.
-            self.weather[self.row_date] = self.temporary_daily_dict
-            self.temporary_daily_dict = {}
-            self.reset_flags()
-
-    def reset_flags(self):
-        """Resets the boolean flags indicating we are at a new row in the table."""    
-        self.in_row = False
-        self.in_row_data = False
-        self.in_row_header = False
-        self.row_column_index = 0
-
-class WeatherScraper:
+class WeatherScraper():
     """Scrapes environment Canada."""
 
     def __init__(self):
         """Instantiates the class and creates the ip found flag"""
 
         super().__init__()
-        self.parser = MyHTMLParser()
+        self.parser = self.MyHTMLParser()
+        self.start_year = 1996 # on the website the earliest year available is 1996
+        self.start_month = 0 # default value of zero will be changed to the eariest month
         self.current_year = ""
         self.end_year = datetime.now().strftime('%Y')
         self.month = ""
+        self.search_url = f'''https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&Day=1&Year={self.start_year}&Month={self.start_month}'''
+        self.weather = {}
 
-    def scrape_weather(self, search_url):
+    def scrape_weather(self):
         """Returns the weather data."""
-        try:           
-            with urllib.request.urlopen(search_url) as response:
-                HTML_DATA = str(response.read())
-                self.parser.feed(HTML_DATA)
+        try:
+            while self.start_year <= int(self.end_year):
+                # if the month is 0 which is our starting value,
+                #set it to 10 which is the earliest month available
+                #on the website the earliest month for 1996 is october (10th month)
+                if self.start_month == 0:
+                    self.start_month = 10
+                elif self.start_month != 12:
+                # if the month isn't 12, increment the month by 1
+                    self.start_month += 1
+                else:
+                    # if the month is 12, increment the year by 1
+                    #and set the month to the start of the year (1)
+                    self.start_year += 1
+                    self.start_month = 1
                 
-                #print(self.parser.weather)
+                self.search_url = f'''https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&Day=1&Year={self.start_year}&Month={self.start_month}'''
+                print(self.search_url)
+                print(f"Current Year: {self.start_year}, current Month: {self.start_month}")
+                
+                with urllib.request.urlopen(self.search_url) as response:
+                    HTML_DATA = str(response.read())
+                    self.parser.feed(HTML_DATA)
+                    
+                    # update our master dictionary with the newly returned data for the start year and month
+                    self.weather.update(self.parser.return_weather_dict())
+            
+                    #print(self.parser.weather)
+            print('{')
+            for date, data in self.weather.items():
+                print(f"    '{date}': {data},")
+            print('}')
         except HTTPError as e:
             print("HTTP Error:", e)
         except URLError as e:
             print("URL Error:", e)
+            
+    class MyHTMLParser(HTMLParser):
+        """The web scraper."""
+
+        def __init__(self):
+            """Instantiates the class and creates the ip found flag"""
+
+            super().__init__()
+            # flag properties
+            self.in_table_body = False
+            self.in_row_header = False
+            self.in_row = False
+            self.in_row_data = False
+            self.end_of_table = False
+            # row column counter
+            self.row_column_index = 0
+
+            # master dictionary
+            self.weather = {}
+            self.daily_temps = {} # dictionary stored inside of the master dictionary.
+
+            # dictionaries to be populated/accessed on each row iteration.
+            self.temporary_daily_dict = {}
+            self.column_temperature_legend = {0:"Max", 1:"Min", 2:"Mean"}
+            # property to store the row date for each row iteration
+            self.row_date = ""
+
+        def handle_starttag(self, tag, attrs):
+            """The tag to begin parsing at."""
+
+            if tag == "tbody":
+                self.in_table_body = True
+
+            if tag == "tr" and self.in_table_body:
+                self.in_row = True
+
+            if self.in_row and tag == "td":
+                self.in_row_data = True
+
+            # find the table row header
+            if tag == "th":
+                for attr, value in attrs:
+                    # if its attribute is scope and the value is row
+                    if attr == "scope" and 'row' in value:
+                        # set the flag
+                        self.in_row_header = True
+
+            # if we're in the table row header
+            if self.end_of_table is False and self.in_row_header is True:
+                # and the element is abbr
+                if tag == "abbr":
+                    for attr, value in attrs:
+                        # if the attribute is title
+                        if attr == "title":
+                            # get the date value and append it to our daily_temps list
+                            converted_date = is_valid_date(value)
+
+                            if converted_date is not None:
+                                self.row_date = converted_date
+
+        def handle_data(self, data):
+            """Look for ip in the data of the element."""
+
+            if self.in_row_header is True:
+                if data == "Sum":
+                    self.end_of_table = True
+                    self.reset_flags()
+
+                    # TODO: make function that accept a dict and prints it out.
+                    # uncomment to print the formatted dictionary
+                    # print('{')
+                    # for date, data in self.weather.items():
+                    #     print(f"    '{date}': {data},")
+                    # print('}')
+
+            # if we're in a data-row (<td>) and our counter is less than 3
+            if (self.end_of_table is False and self.in_row_data is True and self.row_column_index <= 3):
+                if is_float(data) or data == "M":
+                    # we are inside of a <td> element data-row
+
+                    self.temporary_daily_dict[
+                        self.column_temperature_legend.get(self.row_column_index)] = data
+                    self.row_column_index += 1
+
+            # we are only looking for min, max, and mean, which are the first 3 columns of the table.
+            if self.in_row_data is True and self.row_column_index == 3:
+                # if we've hit 3 columns we need to reset our flags to move to the next row element.
+                self.weather[self.row_date] = self.temporary_daily_dict
+                self.temporary_daily_dict = {}
+                self.reset_flags()
+
+        def return_weather_dict(self):
+            """Returns the current dictionary."""
+            return self.weather
+
+        def reset_flags(self):
+            """Resets the boolean flags indicating we are at a new row in the table."""    
+            self.in_row = False
+            self.in_row_data = False
+            self.in_row_header = False
+            self.row_column_index = 0
 
 def is_valid_date(value):
     """Attempts to parse the value as a date in the 'YYYY-MM-DD' format."""
@@ -154,7 +186,7 @@ def is_float(string):
         return False
 
 if __name__ == "__main__":
-    SEARCH_URL = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&EndYear=2018&Day=1&Year=2018&Month=5'
+    #SEARCH_URL = 'https://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1840&Day=1&Year=2018&Month=5'
     weatherScraper = WeatherScraper()
-    weatherScraper.scrape_weather(SEARCH_URL)
+    weatherScraper.scrape_weather()
     
