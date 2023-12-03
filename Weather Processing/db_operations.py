eda1b750b5eac15b5feaebc4c0eb62e52da405ae
Cole Cianflone <colecianflone123@gmail.com>
2023-12-03 14:52:34 -0600

    documentating db_operations.py
    Adding detailed doc strings

--- Weather Processing/db_operations.py
+++ Weather Processing/db_operations.py
@@ -3,12 +3,25 @@
 import sqlite3
 from dbcm import DBCM
 
-
 class DBOperations:
     """The DB operations class."""
 
     def __init__(self):
-        """Setup the context manager, taking a parameter for the name."""
+        """
+        Initialize the database connection and setup the context manager.
+
+        Description:
+        - Sets up the context manager for the database connection by initializing
+        the necessary attributes.
+        - The 'db_name' attribute is set to 'weather_data.sqlite'.
+        - Creates a 'database_context' attribute using the DBCM class with the provided
+        database name.
+        - Initializes 'conn' and 'cursor' attributes to None.
+
+        Raises:
+        - sqlite3.DatabaseError: If there are issues with initializing the database
+        connection or setting up the context manager.
+        """
         try:
             self.db_name = "weather_data.sqlite"
             self.database_context = DBCM(self.db_name)
@@ -19,10 +32,23 @@ class DBOperations:
             print("Error: __init__ initializing DB.")
 
     def initialize_db(self):
-        """Initialize the database."""
+        """
+        Initialize the database by creating necessary tables and indexes if they don't exist.
+
+        Raises:
+        - sqlite3.OperationalError: If there are issues with the database operation,
+        such as table creation failure.
+
+        Description:
+        - Establishes a connection to the database and creates the 'weather' table
+        if it doesn't already exist.
+        - The 'weather' table includes columns for id, sample_date, location, min_temp,
+        max_temp, and avg_temp. The 'sample_date' column is indexed for faster access.
+        - If the table creation or index creation fails due to an OperationalError,
+        it prints an error message indicating the failure to initialize the database.
+        """
         try:
             with self.database_context as cursor:
-                # Make table
                 cursor.execute(
                     """CREATE TABLE IF NOT EXISTS weather (
                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -44,27 +70,28 @@ class DBOperations:
             print("Error: Failed to initialize database.")
 
     # this function can be passed any query, but validate the query before-hand.
-    # Needed for updating the users database using todays date and most recent in DB
     def get_query_data(self, sql_query):
-        # TODO: FIX DOCUMENTATION
         """
-        Method template for fetching data.
+        Performs an SQL Query and retrieves data from the database.
 
-        Parameters:
-
+        Parameters: 
+        - sql_query (str): SQL Query to execute in the database.
 
         Returns:
-        - list of tuples: A list containing tuples representing weather data records.
-         'sample_date', 'location', 'min_temp', 'max_temp', and 'avg_temp'.
+        - list or None: Data fetched from the executed SQL query. 
+        Returns None if an error occurs.
 
         Raises:
-        - OperationalError: If there are issues with the database operation,
-         such as table not existing.
+        - sqlite3.OperationalError: If there are issues with the database operation, 
+        such as the table not existing or other operational errors.
 
-        - If the table 'weather' does not exist in the database,
-         the method attempts to create it.
-        - If table creation fails, returns None.
-
+        Description:
+        - Executes the provided SQL query on the database. If successful, 
+        retrieves and returns the fetched data as a list of tuples.
+        - If an OperationalError occurs, indicating potential issues like a missing table,
+        it attempts to handle it by initializing the database and trying the query again.
+        - If the table 'weather' doesn't exist, it attempts to create it. 
+        If the table creation fails, None is returned.
         """
         try:
             with self.database_context as cursor:
@@ -84,15 +111,32 @@ class DBOperations:
 
     # this function is used for the box plot
     def fetch_monthy_averages(self, start_year=None, end_year=None):
-        # TODO: FIX DOCUMENTATION HERE fetch_monthy_averages
         """
-        Fetch min, mean, max averages from all months in the database.
+        Fetches minimum, mean, and maximum averages for each month within the specified
+        range of years.
+
         Parameters:
+        - start_year (str or None): The starting year for fetching data. If None, the query 
+        fetches from the earliest available year.
+        - end_year (str or None): The ending year for fetching data. If None, the query 
+        fetches up to the latest available year.
 
         Returns:
-        -
-        Raises:
+        - list or None: A list of tuples containing monthly average data (min, mean, max) 
+        within the specified year range. 
+        Returns None if an error occurs or no data is found.
 
+        Raises:
+        - ValueError: If start_year or end_year are not valid digits.
+
+        Description:
+        - Constructs an SQL query to calculate monthly averages for minimum, mean, and maximum 
+        temperatures.
+        - Filters the data based on the provided start_year and end_year, excluding records with 
+        missing or invalid temperature values ('M' or null).
+        - Groups the calculated averages by month and orders the results by month.
+        - Calls the 'get_query_data' method to execute the constructed SQL query and fetch the data
+          from the database.
         """
         if start_year.isdigit() and end_year.isdigit():
             sql_query = (
@@ -117,14 +161,31 @@ class DBOperations:
 
     # this function is used for the line chart
     def fetch_year_month_average(self, year=None, month=None):
-        # TODO: FIX DOCUMENTATION HERE fetch_year_month_average
-
         """
-        Fetch min, mean, max averages for specified year and month from the database.
-        Returns:
-        -
-        Raises:
+        Fetches minimum, mean, and maximum averages for the specified year and month from the 
+        database.
 
+        Parameters:
+        - year (str or None): The year for which data is to be fetched. If None, data for all years
+          is considered.
+        - month (str or None): The month for which data is to be fetched. If None, data for all 
+        months is considered.
+
+        Returns:
+        - list or None: A list of tuples containing daily average data for the specified year and 
+        month. 
+        Returns None if an error occurs or no data is found.
+
+        Raises:
+        - ValueError: If year or month are not valid digits.
+
+        Description:
+        - Constructs an SQL query to calculate daily average temperatures for the specified year 
+        and month.
+        - Filters the data based on the provided year and month.
+        - Groups the calculated averages by day and orders the results by day.
+        - Calls the 'get_query_data' method to execute the constructed SQL query and fetch the data
+          from the database.
         """
         if year.isdigit() and month.isdigit():
             sql_query = (
@@ -142,7 +203,19 @@ class DBOperations:
         return self.get_query_data(sql_query)
 
     def get_earliest_date(self):
-        """Gets earliest date in dataset."""
+        """
+        Retrieves the earliest date available in the dataset.
+
+        Returns:
+        - list or None: A list containing the earliest year and month found in the dataset
+        formatted as ['year', 'month']. Returns None if there is no data.
+
+        Description:
+        - Constructs an SQL query to fetch the minimum date available in the 'sample_date' column.
+        - Calls the 'get_query_data' method to execute the constructed SQL query and fetch 
+        the data from the database.
+        - Parses the retrieved date string to extract the earliest year and month if available.
+        """
         earliest_date_query = """SELECT MIN(strftime('%Y-%m', sample_date)) AS
         earliest_year FROM weather"""
         earliest_year = self.get_query_data(earliest_date_query)[0][0]
@@ -151,14 +224,38 @@ class DBOperations:
         return None
 
     def get_year_ends(self):
-        """ends"""
+        """
+        Retrieves information about the newest and oldest data points in the dataset.
+
+        Returns:
+        - tuple or None: A tuple containing information about the newest and oldest data points.
+        If no data is available, returns None.
+
+        Description:
+        - Checks for the availability of new data and the earliest date in the dataset using
+        the 'get_new_data' and 'get_earliest_date' methods.
+        - If both new data and the earliest date are available, it returns a tuple
+        containing the newest and oldest data points.
+        - If either new data or the earliest date is not available, it returns None.
+        """
         if self.get_new_data() is None and self.get_earliest_date() is None:
             return None
         return (self.get_new_data(), self.get_earliest_date())
 
     def get_new_data(self):
-        """Get the most recent date.
-        Returns 2 string that hold the interger of the year and month"""
+        """
+        Retrieves the most recent date available in the dataset.
+
+        Returns:
+        - list or None: A list containing the most recent year and month found in the dataset
+        formatted as ['year', 'month']. Returns None if there is no data.
+
+        Description:
+        - Constructs an SQL query to fetch the maximum date available in the 'sample_date' column.
+        - Calls the 'get_query_data' method to execute the constructed SQL query and fetch 
+        the data from the database.
+        - Parses the retrieved date string to extract the most recent year and month if available.
+        """
         recent_date_query = (
             "SELECT MAX(strftime('%Y-%m', sample_date)) AS latest_date FROM weather"
         )
@@ -170,13 +267,27 @@ class DBOperations:
 
     def save_data(self, data_to_save):
         """
-        Save new (non duplicate) data to the database.
+        Save new (non-duplicate) data to the database.
 
-        Save new data to the 'weather' table in the database.
+        Saves new data to the 'weather' table in the database.
 
         Parameters:
-        - data_to_save (dict): A dictionary containing data to be saved
-        keys are dates, values are dictionaries with temperature.
+        - data_to_save (dict): A dictionary containing data to be saved.
+        Keys are dates, and values are dictionaries with temperature information
+        (keys: 'Min', 'Max', 'Mean').
+
+        Description:
+        - Attempts to save new data to the 'weather' table in the database.
+        - Loops through the provided 'data_to_save' dictionary, extracting date and 
+        temperature information.
+        - Inserts the extracted data into the 'weather' table if the sample_date 
+        doesn't already exist,
+        using an 'INSERT OR IGNORE' SQL statement.
+        - If there's an OperationalError while saving the data, attempts to handle it 
+        by initializing the database
+        and re-saving the data.
+        - If there's an IntegrityError due to conflicts with existing data during insertion,
+        prints an error message indicating an Integrity Error occurred with the save_data function.
         """
         try:
             with self.database_context as cursor:
@@ -216,11 +327,12 @@ class DBOperations:
         Remove all data from the 'weather' table, or drop all tables in the database.
 
         Parameters:
-        - burn (bool): If True, all tables in the database will be dropped.
-                    If False (default), only data from the 'weather' table will be deleted.
+        - burn (bool): If True, drops all tables in the database.
+                    If False (default), deletes data from the 'weather' table only.
 
         Note:
         - Be cautious when using burn=True, as it will permanently delete all tables and their data.
+        - This method ensures new inserts will start at index 0 after data deletion.
 
         Usage:
         Example 1: obj.purge_data()  # Deletes data from the 'weather' table.
@@ -228,13 +340,11 @@ class DBOperations:
         """
         try:
             with self.database_context as cursor:
-                # Delete data, then auto-increment values, so new inserts start at 0
                 cursor.execute("DELETE FROM weather")
                 cursor.execute("DELETE FROM sqlite_sequence")
 
                 print("Data deleted from weather table.")
 
-                # If true does these too
                 if burn is True:
                     print("Dropping any views, index's found in sqlite_master.")
 
